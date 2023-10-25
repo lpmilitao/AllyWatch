64c498dd5c99f85592e782674e092f143e646260
lpmilitao <luizamilitaop@gmail.com>
2023-10-24 23:54:33 -0300

    adicionando endpoints -> classes, ainda não funcional

---
+++ web/src/external/server/_base/urls.js
@@ -0,0 +1,13 @@
+export const USER_URL = "/user";
+export const LOGOUT_URL = "/logout";
+export const LOGIN_URL = "/login";
+export const POST_URL = "/post";
+export const COMMENT_URL = "/comment";
+export const LIKE_URL = "/like";
+export const REPORT_URL = "/report";
+export const ADMIN_URL = "/admin";
+export const SPECIALIST_URL = "/specialist";
+export const LAWYER_URL = "/lawyer";
+export const PSYCHOLOGIST_URL = "/psychologist";
+export const CHAT_URL = "/chat";
+export const SOLICITATION_URL = "/solicitation";
