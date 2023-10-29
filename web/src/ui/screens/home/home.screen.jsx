8f2a88050750b6b922737c663e1f8c8cf1a041a3
Luiza <lpmilitao>
2023-10-29 15:32:22 -0300

    adicionando primeira parte da tela home

--- web/src/ui/screens/home/home.screen.jsx
+++ web/src/ui/screens/home/home.screen.jsx
@@ -1,11 +1,23 @@
 import './home.style.css';
 
 import image from '../../../assets/images/Home.png';
+import arrow from '../../../assets/icons/arrow-white.svg';
+import { useNavigate } from 'react-router-dom';
 
 export function Home() {
+  const navigate = useNavigate();
+
   return (
     <section className='home'>
+      <button className='home-button' onClick={() => navigate('/signup')}>
+        <img src={arrow} alt='' className='left' />
+        <p>Cadastro</p>
+      </button>
       <img src={image} alt='' />
+      <button className='home-button' onClick={() => navigate('/signin')}>
+        <p>Login</p>
+        <img src={arrow} alt='' />
+      </button>
     </section>
   );
 }
