f24407c616ee149a03f1b2308740b31c1e74132f
Luiza Militão Pereira <luizamilitao>
2023-11-03 01:43:26 -0300

    adicionando tela default para cadastro de especialista

--- web/src/ui/components/back/back.component.jsx
+++ web/src/ui/components/back/back.component.jsx
@@ -5,12 +5,21 @@ import { useNavigate } from 'react-router-dom';
 import pink from '../../../assets/icons/short-arrow-pink.svg';
 import white from '../../../assets/icons/short-arrow-white.svg';
 
-export function Back({ mode, route }) {
+export function Back({ mode, route, side }) {
   const navigate = useNavigate();
 
   return (
-    <div className='back-button' onClick={() => navigate(route ? route : -1)}>
-      <img src={mode === 'white' ? white : pink} alt='Pink arrow' />
+    <div
+      className={
+        side === 'right' ? 'back-button right-side' : 'back-button left-side'
+      }
+      onClick={() => navigate(route ? route : -1)}
+    >
+      <img
+        src={mode === 'white' ? white : pink}
+        alt='Pink arrow'
+        className={side === 'right' ? 'mirror' : ''}
+      />
       <p>Voltar</p>
     </div>
   );
--- web/src/ui/screens/signup/specialist/specialistSignUp.screen.jsx
+++ web/src/ui/screens/signup/specialist/specialistSignUp.screen.jsx
@@ -1,3 +1,58 @@
 import './specialistSignUp.style.css';
 
-export function SpecialistSignUp() {}
+import { useEffect, useState } from 'react';
+
+import specialist from '../../../../assets/images/specialist.png';
+
+import { Back, Form } from '../../../components';
+
+const lawyer = {
+  title: 'Advogado',
+};
+const psychologist = {
+  title: 'Psicólogo',
+};
+
+export function SpecialistSignUp() {
+  const [type, setType] = useState('specialist');
+
+  useEffect(() => {}, [type]);
+
+  return (
+    <>
+      <Back side={'right'} />
+      <section className='specialist-signup-container'>
+        {type === 'lawyer' ? (
+          <Form>
+            <h1>{lawyer.title}</h1>
+          </Form>
+        ) : type === 'psychologist' ? (
+          <Form>
+            <h1>{psychologist.title}</h1>
+          </Form>
+        ) : (
+          <div className='specialist-presentation'>
+            <h1>Seja a voz de apoio de quem precisa</h1>
+            <span>
+              Cadastre-se como psicólogo ou advogado em nossa plataforma e
+              ofereça seu auxílio aos que necessitam de orientação e suporte
+              diante do assédio moral e violência sexual escolar.
+              <br />
+              <p>Juntos, fazemos a diferença.</p>
+            </span>
+            <img src={specialist} alt='' />
+          </div>
+        )}
+        <div className='pick'>
+          <h1>Sou um</h1>
+          <button onClick={() => setType('lawyer')}>
+            <span>Advogado</span>
+          </button>
+          <button onClick={() => setType('psychologist')}>
+            <span>Psicólogo</span>
+          </button>
+        </div>
+      </section>
+    </>
+  );
+}
